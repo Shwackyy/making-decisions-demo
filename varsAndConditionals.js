eb9a93988d4cd72274af7c5953ec6823f28db38e
Brett Stevens <brettstevens899@yahoo.com>
2022-02-09 13:26:30 -0700

    Completed Demo

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -3,3 +3,53 @@
     and loops to play out a fight between Jon Snow and
     Jamie Lannister.
 */ 
+
+let eowynAttack = 50;
+let witchKingAttack = 45;
+
+if(eowynAttack > witchKingAttack) {
+    console.log("eowyn is stronger");
+} else if(witchKingAttack > eowynAttack){
+    console.log("witch King is stronger");
+} else {
+    console.log("they are the same strength");
+}
+
+let eowynHealth = 100;
+let eowynDefense = 0;
+
+if(eowynHealth + eowynDefense <= witchKingAttack) {
+    console.log("she dead.");
+} else {
+    //eowynHealth = eowynHealth - witchKingAttack;
+    console.log("she alive");
+}
+
+
+
+let coinLandsHeads = false;
+
+if(coinLandsHeads === true) {
+    console.log("Witch King gets to run away.");
+} else {
+    console.log("Witch King got stabbed in the face.");
+};
+
+// for(let i = 0; i < 5; i++) {
+//     if(eowynHealth > 0) {
+//         eowynHealth = eowynHealth - witchKingAttack;
+//         console.log(`Eowyn has ${eowynHealth} left.`);
+//     } else {
+//         console.log(`Eowyn dead.`);
+//     };
+// };
+
+
+
+while(eowynHealth > 0) {
+    eowynHealth -= witchKingAttack
+    console.log(`Eowyn has ${eowynHealth} left.`)
+    if(eowynHealth <= 0) {
+        console.log("Eowyn has been slain.")
+    }
+}
